de874223f947d4d43f7d3be1804f11ae6d282af6
Clint Dev <cmaruti93@gmail.com>
2019-07-20 17:54:05 +0300

    added a logger(morgan), nodemon and error handling

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productsRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
 
 app.use((req,res,next) => {
-    res.status(200).json({
-        message: 'It works!!'
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error,req,res,next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
     });
 });
 
