91e2b5c6de2c6b1988c35a55a0b9f6b0f757e53e
Clint Dev <cmaruti93@gmail.com>
2019-07-20 15:14:08 +0300

    Added more routes

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const productsRoutes = require('./api/routes/products');
+const ordersRoutes = require('./api/routes/orders');
+
+app.use('/products', productsRoutes);
+app.use('/orders', ordersRoutes);
+
 app.use((req,res,next) => {
     res.status(200).json({
         message: 'It works!!'
